db313a6c21672a8409758dffd38e2d275e6f0fcc
Ahmed Tork <torkahmed93@gmail.com>
2018-04-09 18:58:44 +0200

    Initial Commit

---
+++ PiTest/PiTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PiTest
+//
+//  Created by Ahmed Tourk on 4/9/18.
+//  Copyright © 2018 AhmedTourk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
