e8015ff7b1145e313d0a17097547e022cc9c8565
Ahmed Tourk <ahmedtourk@Ahmeds-MacBook-Pro.local>
2018-08-22 20:48:47 +0200

    Adding Hoda's greet message

--- PiTest/PiTest/main.c
+++ PiTest/PiTest/main.c
@@ -8,9 +8,32 @@
 
 #include <stdio.h>
 
+
+void greetHoda(char *str)
+{
+	printf("%s\n", str);
+}
+
+
+void calc(int a, int b, int *add, int *product)
+{
+    *add = a+b;
+    *product = a*b;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int x;
+    int y;
+    
     printf("Hello, World!\n");
     printf("Welcome to my World!\n");
+    calc(2,5,&x,&y);
+    printf("2 + 5 = %d\n2 x 5 = %d\n", x, y);
+    printf("This Commit is added to Trello Board!\n");
+
+    greetHoda("Welcome to MacBook ya Hoda");
+
+
     return 0;
 }
